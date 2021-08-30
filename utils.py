fb6276f1ffdeb2e35832ca41c674e96bf53cf659
StevenJobs <StevenJobs@apple.com>
2021-08-30 11:12:27 +0800

    bug fixed

--- utils.py
+++ utils.py
@@ -7,18 +7,19 @@ import xlwt
 import shutil
 
 
+def z_log(value):
+    print(value)
+    print('\n')
+
+
 def r_newline(str):
     return str.replace('\n', '').replace('\r', '')
 
 
-def z_split(str, sls, srs):
-    sls_len = len(sls)
-    if sls_len != len(srs):
-        return
-    for i in range(sls_len):
-        (sl, sr) = (sls[i], srs[i])
-        str = str.replace(sl + sr, sl+'<->')
-    return str.split('<->')
+def lines_in_js(str):
+    str_insert_line_char = str.replace(
+        '\',\n', '\'<--line-->').replace('",\n', '"<--line-->').replace('`,\n', '`<--line-->')
+    return r_newline(str_insert_line_char).split('<--line-->')
 
 
 def z_arr_push(arr, value):
@@ -55,15 +56,15 @@ def search_files(path, filter):
 def read_js_json(js_file):
     with open(js_file, 'r') as file:
         js_str = file.read()
-        js_str = js_str[js_str.find('{') + 1: js_str.find('}')]  # 去除 {}
-        js_str = r_newline(js_str)  # 去除换行
-        local_str_list = z_split(js_str, ['\'', '`'], [',', ','])
+        js_str = js_str[js_str.find('{') + 1: js_str.rfind('}')]  # 去除 {}
+        local_str_list = lines_in_js(js_str)  # 插入行标，并去除换行
         local_kv_list = []
         for local_str in local_str_list:
             if len(local_str) == 0:
                 continue
             str = r_newline(local_str.strip())
-            [key, value] = z_split(str, '\'', ':')
+            kvArr = str.split(':', 1)
+            [key, value] = kvArr
             if key.find('//') != -1:
                 ls = key.split(' ')
                 key = ls[len(ls) - 1]
